431d8291864b3f74595e631c94095154279a5192
Sahad Mahaboob <sahadmahaboob@gmail.com>
2023-07-09 18:43:55 +0530

    made form connection to google sheet

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ if(close) {
       nav.classList.remove('active');
     })
 };
+
+// -----------Google form--------------
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycby7BwW1ae3ONzpKFJz9RPlhkh2P9Rkk43x1gKAarmPcktxexxZp5P_vqw9tWzqXA8pA3w/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+})
